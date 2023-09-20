56cd6cc63b5bd665645a3ba0bd6b1e8c75faf15e
Nathan Cobb <nathanrcobb@gmail.com>
2023-09-19 19:21:28 -0700

    Making progress, finished nav/headline/greeting

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def all(request):
+  blogs = Blog.objects.order_by('-date')
+
+  # TODO: Deal with pagination
+
+  return render(request, 'blog/all.html', {
+    'blogs': blogs
+  })
+
+
+def detail(request, blog_id):
+  blog = get_object_or_404(Blog, pk=blog_id)
+
+  return render(request, 'blog/detail.html', {
+    'blog': blog
+  })
--- personal_portfolio-project/portfolio/admin.py
+++ personal_portfolio-project/portfolio/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Project, Tech, TechUsage
+from blog.models import Blog
 
 admin.site.register(Project)
 admin.site.register(Tech)
 admin.site.register(TechUsage)
+admin.site.register(Blog)
