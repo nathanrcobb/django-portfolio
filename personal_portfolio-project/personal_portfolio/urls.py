01fc33bb0ee7d7fa2deb1ea6abfd2e9d675f5616
Nathan Cobb <nathanrcobb@gmail.com>
2023-09-22 17:29:09 -0700

    A few more miscellaneous updates

--- personal_portfolio-project/personal_portfolio/urls.py
+++ personal_portfolio-project/personal_portfolio/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'portfolio.views.handler404'
 handler500 = 'portfolio.views.handler500'
